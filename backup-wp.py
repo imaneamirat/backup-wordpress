bad3c4b51aa138b3ea6990b63ab5f035271bd0a7
imaneamirat <imaneamirat08@gmail.com>
2021-09-24 19:14:46 +0200

    Add FTP copy

--- backup-wp.py
+++ backup-wp.py
@@ -4,14 +4,18 @@
 #
 # This python script is used to backup Wordpress website and associated mysql database
 # using mysqldump and tar utility.
-# Backups are copied to AWS S3 and encrypted using a private AES-256 key
+# Backups are copied to either :
+# - AWS S3 and encrypted using a private AES-256 key
+# or
+# - FTP server
+#
 # This scripts needs root privileges
 #
 # Written by : Imane AMIRAT
 # Created date: Sept 21, 2021
-# Last modified: Aug 21, 2021
+# Last modified: Aug 24, 2021
 # Tested with : Python 3.8
-# Script Revision: 0.3
+# Script Revision: 0.5
 #
 ##########################################################
 
@@ -26,11 +30,42 @@ import configparser
 import tarfile
 import boto3
 from botocore.config import Config
+from ftplib import FTP
 
 # By Default, this script will read configuration from file /etc/backup-wp.conf
 # Todo : Add the option -f to read parameters from a specified filename in the command line parameter
+# Todo : Backup Folder Rotation Strategy
+'''
+Backup folders :
 
-CONFIG_FILE = '/etc/backup-wp.conf'
+Create the following folders :
+
+/data/backup/dayJ
+/data/backup/dayJ-1
+/data/backup/dayJ-2
+/data/backup/dayJ-3
+/data/backup/dayJ-4
+/data/backup/dayJ-5
+/data/backup/dayJ-6
+/data/backup/dayJ-7
+
+Before each new daily backup  :
+
+1) Rotation :
+
+/data/backup/day-J-7 rm files
+/data/backup/day-J-6 mv to /data/backup/dayJ-7
+/data/backup/day-J-5 mv to /data/backup/dayJ-6
+/data/backup/day-J-4 mv to /data/backup/dayJ-5
+/data/backup/day-J-3 mv to /data/backup/dayJ-4
+/data/backup/day-J-2 mv to /data/backup/dayJ-3
+/data/backup/day-J-1 mv to /data/backup/dayJ-2
+/data/backup/day-J mv to /data/backup/dayJ-1
+
+2) copy new backup files in /data/backup/dayJ
+'''
+
+CONFIG_FILE = "/etc/backup-wp.conf"
 
 '''
 Example of config file content :
@@ -44,10 +79,20 @@ DB_USERNAME=wpu
 DB_PASSWORD=Imane$2021!
 [BACKUP]
 LOCALBKPATH=/data/backup
+BACKUP_DEST=S3
 S3_BUCKET=ImaneAIC-WP
 S3_ACCESS_KEY=XXXXXXXXXXX
 S3_SECRET_ACCESS_KEY=YYYYYYYY
 S3_DEFAULT_REGION=eu-west-3
+
+Or
+[BACKUP]
+LOCALBKPATH=/data/backup
+BACKUP_DEST=FTP
+FTP_SERVER=ftp.imaneaic.com
+FTP_USER=backupwp
+FTP_PASSWD=1edd!ai3$
+FTP_PATH=backup-wp
 '''
 
 config = configparser.ConfigParser()
@@ -58,12 +103,24 @@ DB_HOST = config.get('DB','DB_HOST')
 DB_NAME = config.get('DB','DB_NAME')
 DB_USERNAME = config.get('DB','DB_USERNAME')
 DB_PASSWORD = config.get('DB','DB_PASSWORD')
+
+BACKUP_DEST = config.get('BACKUP','BACKUP_DEST')
 BACKUP_PATH = config.get('BACKUP','LOCALBKPATH')
 
-S3_BUCKET = config.get('BACKUP','S3_BUCKET')
-S3_ACCESS_KEY = config.get('BACKUP','S3_ACCESS_KEY')
-S3_SECRET_ACCESS_KEY = config.get('BACKUP','S3_SECRET_ACCESS_KEY')
-S3_DEFAULT_REGION = config.get('BACKUP','S3_DEFAULT_REGION')
+
+if BACKUP_DEST == 'S3':
+    S3_BUCKET = config.get('BACKUP','S3_BUCKET')
+    S3_ACCESS_KEY = config.get('BACKUP','S3_ACCESS_KEY')
+    S3_SECRET_ACCESS_KEY = config.get('BACKUP','S3_SECRET_ACCESS_KEY')
+    S3_DEFAULT_REGION = config.get('BACKUP','S3_DEFAULT_REGION')
+elif BACKUP_DEST == 'FTP':
+    FTP_SERVER = config.get('BACKUP','FTP_SERVER')
+    FTP_USER = config.get('BACKUP','FTP_USER')
+    FTP_PASSWD = config.get('BACKUP','FTP_PASSWD')
+    FTP_PATH = config.get('BACKUP','FTP_PATH')
+else:
+    print("Bad value in " + CONFIG_FILE + ". Value of BACKUP_DEST should be S3 or FTP only. Exiting")
+    exit(1)
 
 # Getting current DateTime to create the separate backup folder like "20210921".
 DATETIME = time.strftime('%Y%m%d')
@@ -105,33 +162,55 @@ print ("")
 print ("Backup of  Wordpress Site folder completed")
 
 
-# Part 3 : Copy to S3
+# Part 3 : Copy to BACKUP_DEST
 
-print ("")
-print ("Starting Copy to AWS S3")
+if BACKUP_DEST == 'S3':
+    print ("")
+    print ("Starting Copy to AWS S3")
 
-bucket_name = S3_BUCKET # name of the bucket
+    bucket_name = S3_BUCKET # name of the bucket
 
-my_config = Config(
-    region_name = S3_DEFAULT_REGION,
-    retries = {
-        'max_attempts': 10,
-        'mode': 'standard'
-    }
-)
+    my_config = Config(
+        region_name = S3_DEFAULT_REGION,
+        retries = {
+            'max_attempts': 10,
+            'mode': 'standard'
+        }
+    )
 
 
-s3_client = boto3.client(
-    's3',
-    aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_ACCESS_KEY, 
-    config=my_config
-)
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=S3_ACCESS_KEY,
+        aws_secret_access_key=S3_SECRET_ACCESS_KEY, 
+        config=my_config
+    )
 
-for file_name in [localMysqlBackup,wp_archive]:
-    finaldest=DATETIME + "/" + os.path.basename(file_name)
-    s3_client.upload_file(file_name, S3_BUCKET, finaldest)
+    for file in [localMysqlBackup,wp_archive]:
+        file_name=DATETIME + "/" + os.path.basename(file)
+        s3_client.upload_file(file, S3_BUCKET, file_name)
+
+    print ("")
+    print ("Copy to AWS S3 completed")   
+    
+else:
+    print ("")
+    print ("Starting Copy to FTP Server")    
+    ftp = FTP(FTP_SERVER)
+    ftp.login(user=FTP_USER,passwd=FTP_PASSWD)
+    ftp.cwd(FTP_PATH)
+
+    for file in [localMysqlBackup,wp_archive]:
+        file_name = os.path.basename(file)
+        file_descriptor = open(file,'rb')
+        CMD = "STOR " + file_name
+        ftp.storbinary(CMD, file_descriptor)
+
+    print ("")
+    print ("Copy to FTP Server completed")   
+
+
 
 print ("")
 print ("Backup script completed")
-print ("Your backups have been created in '" + TODAYBACKUPPATH + "' directory")
+print ("Your backups have also been created locally in '" + TODAYBACKUPPATH + "' directory")
